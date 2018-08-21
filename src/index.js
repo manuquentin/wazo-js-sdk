288f7447e018ac994eb55087cd9d145ec70feff3
Sylvain Boily <sylvainboilydroid@gmail.com>
2018-08-21 00:13:48 -0400

    add answer

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import logOut from './api/auth/log-out';
 
 import calls from './api/applications/calls';
 import hangupCall from './api/applications/hangup-call';
+import AnswerCall from './api/applications/answer-call';
 
 export default {
   client,
@@ -15,4 +16,5 @@ export default {
   logOut,
   calls,
   hangupCall,
+  AnswerCall,
 };
