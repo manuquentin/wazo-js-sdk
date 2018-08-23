37a3cc34e9d8b668ecf0d99d15af980b0aa4fecb
Sylvain Boily <sylvainboilydroid@gmail.com>
2018-08-23 10:51:34 -0400

    add nodes actions

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import calls from './api/applications/calls';
 import hangupCall from './api/applications/hangup-call';
 import answerCall from './api/applications/answer-call';
 import playCall from './api/applications/play';
+import listNodes from './api/applications/nodes';
+import listCallsNodes from './api/applications/nodes-calls';
+import removeCallNodes from './api/applications/nodes-remove-calls';
 
 import WebRTCPhone from './api/phone';
 
@@ -20,6 +23,9 @@ export default {
   calls,
   hangupCall,
   answerCall,
+  listNodes,
+  listCallsNodes,
+  removeCallNodes,
   playCall,
   WebRTCPhone,
 };
